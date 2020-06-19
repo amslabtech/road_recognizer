a579411ec1eafc62b826c6172689d83b9233ced0
Kazuki <tkhsh.schoen@gmail.com>
2020-06-19 21:40:05 +0900

    add test file

---
+++ test/src/intersection_detection_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "road_recognizer/intersection_detector.h"
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
