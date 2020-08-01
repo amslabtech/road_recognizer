e3c73c79d91c886529ecb1111752d9a1fac1e020
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2020-08-01 17:30:17 +0900

    remove unnecessary #include

--- include/road_recognizer/velodyne_differentiator.h
+++ include/road_recognizer/velodyne_differentiator.h
@@ -19,7 +19,6 @@
 // OpenCV
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 
 // Eigen
